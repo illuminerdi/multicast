d46f731d6def90f2ece90021182198d45002f05b
Joshua Clingenpeel <joshua.clingenpeel@gmail.com>
2009-05-12 18:50:10 -0700

    simple multicast script in place. able to subscribe to a server that's multicasting to all systems on the subnet.

--- lib/multicast.rb
+++ lib/multicast.rb
@@ -5,23 +5,31 @@ require 'timeout'
 
 class Multicast
   VERSION = '1.0.0'
-  
+  ADDR = '224.0.0.1'
+  PORT = '7387'
   def self.server
-    server = UDPSocket.new
-    server.bind nil, 12321
-    loop do
-      text, sender = server.recvfrom(1)
-      server.send("#{Time.now}\n",0,sender[3],sender[1])
+    begin
+      server = UDPSocket.open
+      loop do
+        print "What to say: "
+        text = gets.chomp
+        server.send("#{text}\n",0, ADDR, PORT)
+        puts "You said: #{text}"
+      end
+    ensure
+      server.close
     end
   end
-  
   def self.client
-    socket = UDPSocket.new 
-    socket.connect('localhost', 12321) 
-    socket.send('', 0) 
-    timeout(10) do 
-      time = socket.gets 
-      puts time 
+    begin
+      socket = UDPSocket.new 
+      socket.bind('', PORT)
+      loop do 
+        recvd, info = socket.recvfrom(1024) 
+        puts "Received: '#{recvd.strip}' from [#{info[2]}]" unless recvd.chomp.empty?
+      end
+    ensure
+      socket.close
     end
   end
 end
